74ca981d6dc2befd222edffaf2d3544dfe1c3ec2
Ruan Kenedi <ruanv1d4@gmail.com>
2017-07-20 12:42:17 +0000

    First commit

---
+++ src/components/Flags.vue
@@ -0,0 +1,67 @@
+<template>
+  <div style="width: 215px; max-width: 90vw; margin-left: 15px;">
+    <q-input id="label" color="amber" v-model="terms" float-label="Label">
+      <q-autocomplete @search="search" :min-characters="1" @selected="selected" :static-data="{field: 'value', list: flags}"/>
+    </q-input>
+  </div>
+</template>
+
+<script>
+import {  QInput, QSelect, QIcon, QAutocomplete, QSearch } from 'quasar'
+
+export default {
+  components: {
+    QAutocomplete,
+    QSearch,
+    QIcon,
+    QInput
+  },
+  props:{
+
+  },
+  data () {
+    return {
+      countries: [
+        {
+          code: 55,
+          shortName: 'BR',
+          name: 'Brasil'
+        },
+        {
+          code: 376,
+          shortName: 'AD',
+          name: 'Andorra'
+        }
+      ],
+
+      selected: '',
+      terms: ''
+    }
+  },
+  computed: {
+    flags() {
+      return this.countries.map((country) => {
+        return {
+          avatar: require(`../assets/flags/4x3/${country.shortName.toLowerCase()}.svg`),
+          label: country.name,
+          value: country.shortName,
+          stamp: `+${country.code}`
+        };
+      })
+    }
+  },
+  methods: {
+    search (terms, done) {
+      setTimeout(() => {
+        done(filter(terms, {field, list}))
+      }, 100)
+    },
+  }
+}
+</script>
+
+ <style>
+
+ </style>
+
+
---
+++ src/components/Phone.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="layout-padding row justify-center">
+      <q-input id="phone" float-label="Phone" style="width: 215px;"/>
+    <div style="width: 215px; max-width: 90vw; margin-left: 15px;">
+       <q-input id="label" color="amber" v-model="terms" float-label="Label">
+          <q-autocomplete @search="search" :min-characters="1" @selected="selected" :static-data="{field, list}"/>
+       </q-input>
+    </div>
+  </div>
+</template>
+
+<script>
+
+// function startLabel() {
+//   if() {
+
+//   }
+// }
+
+import {
+  QAutocomplete,
+  QSearch,
+  QInput,
+  uid,
+  filter,
+  Toast,
+  QSelect,
+  QIcon
+} from 'quasar'
+
+export default {
+  components: {
+    QAutocomplete,
+    QSearch,
+    QInput,
+    QSelect,
+    QIcon
+  },
+  data () {
+    return {
+      terms: '',
+      field: 'value',
+        list: [
+          {
+            value: 'Residential', // The value given, when selected
+            label: 'Residential', // The value displayed as main label for this suggested selection
+          },
+          {
+            value: 'Commercial', // The value given, when selected
+            label: 'Commercial', // The value displayed as main label for this suggested selection
+          },
+          {
+            value: 'Cell Phone', // The value given, when selected
+            label: 'Cell Phone', // The value displayed as main label for this suggested selection
+          },
+          {
+            value: 'Other', // The value given, when selected
+            label: 'Other', // The value displayed as main label for this suggested selection
+          }
+        ],
+    }
+  },
+  methods: {
+    search (terms, done) {
+      setTimeout(() => {
+        done(filter(terms, {field, list}))
+      }, 100)
+    },
+  }
+}
+</script>
